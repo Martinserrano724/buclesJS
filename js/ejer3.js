91726c93514fa25baf7da106a0288647826bddd2
Martinserrano724 <martinserrano724@gmail.com>
2023-03-04 15:27:00 -0300

    Desarrollo del ejercicio 3

---
+++ js/ejer3.js
@@ -0,0 +1,16 @@
+/*
+3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. 
+Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
+
+Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp
+
+Dific
+
+*/
+let cad1 ='';
+while(confirm("Presiona  enter para ingreasar una cadena\n  Cancelar Para Dejar de ingresar.")){
+cad1 = cad1+' - '+prompt("Ingrese una cadena de texto");
+}
+document.write(`la cadenas ingresadas son:\n${cad1}`);
+
+
