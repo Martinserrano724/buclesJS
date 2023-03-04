a9f34799a8caf4f33cc4b1d8236f01c4811d620a
Martinserrano724 <martinserrano724@gmail.com>
2023-03-04 15:44:15 -0300

    Desarrollo del ejercicio 4

--- js/ejer3.js
+++ js/ejer3.js
@@ -7,7 +7,7 @@ Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp
 Dific
 
 */
-let cad1 ='';
+let cad1 =prompt("Ingrese una cadena de texto");
 while(confirm("Presiona  enter para ingreasar una cadena\n  Cancelar Para Dejar de ingresar.")){
 cad1 = cad1+' - '+prompt("Ingrese una cadena de texto");
 }
