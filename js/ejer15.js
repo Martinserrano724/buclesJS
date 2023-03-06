f48a4273b1cad9e2da4caf5d4b2d0fea5206e894
Martinserrano724 <martinserrano724@gmail.com>
2023-03-06 10:47:19 -0300

    Creacion del ejercicio 15

---
+++ js/ejer15.js
@@ -0,0 +1,21 @@
+/* *15- Realiza un script que cuente el número de vocales que tiene un texto.
+ */
+let cadena = prompt("Ingrese la cadea de texto");
+let contador = 0;
+for (let index = 0; index < cadena.length; index++) {
+  if (
+    (cadena[index] = "A") ||
+    (cadena[index] = "a") ||
+    (cadena[index] = "e") ||
+    (cadena[index] = "E") ||
+    (cadena[index] = "i") ||
+    (cadena[index] = "I") ||
+    (cadena[index] = "O") ||
+    (cadena[index] = "o") ||
+    (cadena[index] = "U") ||
+    (cadena[index] = "u")
+  ) {
+    contador++;
+  }
+}
+document.write(`El numero de vocales es: ${contador} de la cadena: ${cadena}`)
