ba5bd75799c5aef0a1920fbd3fc9f306eeab3585
Martinserrano724 <martinserrano724@gmail.com>
2023-03-06 10:55:42 -0300

    Desarrollo del ejercicio 16

--- js/ejer16.js
+++ js/ejer16.js
@@ -1,3 +1,9 @@
 /*16- Realiza un script que pida una cadena de texto y la devuelva al revés.
  Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.
  */
+ let cadena = prompt("Ingrese la cadea de texto");
+ let Cadena2 = '';
+ for (let index = cadena.length; index > 0; index--) {
+    Cadena2+=cadena[index-1]
+ }
+ document.write(`La cadena ingresada es: ${cadena} la cadena es: ${Cadena2}`)
