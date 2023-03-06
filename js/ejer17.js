6785e0d419513d1ea4e8d6aa2776085ba2ae314b
Martinserrano724 <martinserrano724@gmail.com>
2023-03-06 11:03:30 -0300

    Desarrollo del ejercicio 17

--- js/ejer17.js
+++ js/ejer17.js
@@ -2,3 +2,23 @@
 introducido por teclado.
 
 */
+let cadena = prompt("Ingrese la cadea de texto");
+let contador = 0;
+for (let index = 0; index < cadena.length; index++) {
+    
+  if (
+   ( (cadena[index] == "A") ||
+    (cadena[index] == "a") ||
+    (cadena[index] == "e") ||
+    (cadena[index] == "E") ||
+    (cadena[index] == "i") ||
+    (cadena[index] == "I") ||
+    (cadena[index] == "O") ||
+    (cadena[index] == "o") ||
+    (cadena[index] == "U") ||
+    (cadena[index] == "u")) && contador==0
+  ) {
+    contador= (index+1);
+  }
+}
+document.write(`La posicion de la Primer vocal es: '${contador}' de la cadena: '${cadena}'`);
