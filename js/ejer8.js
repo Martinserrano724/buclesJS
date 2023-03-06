eda26a816fca54e67ff0fa7939ae7d4a3385a4c8
Martinserrano724 <martinserrano724@gmail.com>
2023-03-06 09:46:06 -0300

    Desarrollo del ejercicio 8

--- js/ejer8.js
+++ js/ejer8.js
@@ -7,3 +7,24 @@
 12345
 123456
  */
+
+let num1 = prompt(`ingresar el numero de piramide menor a 50`);
+
+if (isNaN(num1)) {
+  alert("El dato ingresado No Es Un Numero.\nVolver a ingresar");
+} else {
+    if(num1 <= 50){
+       
+for (let i = 0; i < num1; i++) {
+    for (let j = 0; j < i; j++) {
+     document.write(j+1);
+    }
+    document.write(`<br>`);
+     
+ }
+    }
+    else{
+        alert("El dato ingresado Es mayor a 50");
+    }
+ 
+}
